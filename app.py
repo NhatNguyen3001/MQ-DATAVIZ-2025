2643b31e81aac2e553c2445da7b6c76730bf9aaa
Nguyen Dang Nhat <73870894+NhatNguyen3001@users.noreply.github.com>
2025-09-20 19:38:51 +1000

    refactor: adjust the image path

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import logo_config
 from navigation import navigation_bar
 
-st.set_page_config(page_title="DataViz_2025", page_icon="../img/short_logo.png", layout="wide", initial_sidebar_state="auto")
+st.set_page_config(page_title="DataViz_2025", page_icon="img/short_logo.png", layout="wide", initial_sidebar_state="auto")
 
 navigation_bar()
 logo_config()
