2c12e6b8cfe104eb0b2eb64943f844f483428f1d
Nguyen Dang Nhat <73870894+NhatNguyen3001@users.noreply.github.com>
2025-09-21 19:26:47 +1000

    refactor: adjust function to import data

--- page_dashboard.py
+++ page_dashboard.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from utils import load_data
+
+
+df = load_data("data/who_ambient_air_quality_database_version_2024.xlsx", "data")
 
 
 st.title("This is the dashboard page")
--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 
 @st.cache_data
-def load_data(df):
-    data = pd.read_csv(df)
+def load_data(df, sheet_name):
+    data = pd.read_excel(df, sheet_name=sheet_name)
     return data
 
 def logo_config():
